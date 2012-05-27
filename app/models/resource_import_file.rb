f3d65bb9824b7ba8a1bb56b497dc382973eb7903
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-05-28 06:24:48 +0900

    updated accessible attributes

--- app/models/resource_import_file.rb
+++ app/models/resource_import_file.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 class ResourceImportFile < ActiveRecord::Base
-  attr_accessible :resource_import
+  attr_accessible :resource_import, :edit_mode
   include ImportFile
   default_scope :order => 'id DESC'
   scope :not_imported, where(:state => 'pending')
