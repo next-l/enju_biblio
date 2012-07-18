56a37caa045e0b48553252700d4e1b3b0132afe9
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-07-18 16:58:27 +0900

    updated accessible attributes

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -8,7 +8,8 @@ class Patron < ActiveRecord::Base
     :telephone_number_1, :telephone_number_2, :fax_number_1, :fax_number_2,
     :other_designation, :place, :street, :locality, :region, :language_id,
     :country_id, :patron_type_id, :note, :required_role_id, :email, :url,
-    :full_name_alternative_transcription, :title, :birth_date, :death_date
+    :full_name_alternative_transcription, :title, :birth_date, :death_date,
+    :patron_identifier
 
   scope :readable_by, lambda{|user| {:conditions => ['required_role_id <= ?', user.try(:user_has_role).try(:role_id) || Role.where(:name => 'Guest').select(:id).first.id]}}
   has_many :creates, :dependent => :destroy
