56e4170f704063ccbb2169015598c146dd8a3345
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-05-05 06:51:06 +0900

    updated nested_form to carrier_type

--- app/controllers/carrier_types_controller.rb
+++ app/controllers/carrier_types_controller.rb
@@ -1,14 +1,94 @@
-class CarrierTypesController < InheritedResources::Base
-  respond_to :html, :json
-  has_scope :page, :default => 1
+class CarrierTypesController < ApplicationController
   load_and_authorize_resource
+  before_filter :prepare_options, :only => [:new, :edit]
 
+  # GET /carrier_types
+  # GET /carrier_types.json
+  def index
+    @carrier_types = CarrierType.order(:position).page(params[:page])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @carrier_types }
+    end
+  end
+
+  # GET /carrier_types/1
+  # GET /carrier_types/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @carrier_type }
+    end
+  end
+
+  # GET /carrier_types/new
+  # GET /carrier_types/new.json
+  def new
+    @carrier_type = CarrierType.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @carrier_type }
+    end
+  end
+
+  # GET /carrier_types/1/edit
+  def edit
+  end
+
+  # POST /carrier_types
+  # POST /carrier_types.json
+  def create
+    @carrier_type = CarrierType.new(params[:carrier_type])
+
+    respond_to do |format|
+      if @carrier_type.save
+        format.html { redirect_to @carrier_type, :notice => t('controller.successfully_created', :model => t('activerecord.models.carrier_type')) }
+        format.json { render :json => @carrier_type, :status => :created, :location => @carrier_type }
+      else
+        prepare_options
+        format.html { render :action => "new" }
+        format.json { render :json => @carrier_type.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /carrier_types/1
+  # PUT /carrier_types/1.json
   def update
-    @carrier_type = CarrierType.find(params[:id])
     if params[:move]
       move_position(@carrier_type, params[:move])
       return
     end
-    update!
+
+    respond_to do |format|
+      if @carrier_type.update_attributes(params[:carrier_type])
+        format.html { redirect_to @carrier_type, :notice => t('controller.successfully_updated', :model => t('activerecord.models.carrier_type')) }
+        format.json { head :no_content }
+      else
+        prepare_options
+        format.html { render :action => "edit" }
+        format.json { render :json => @carrier_type.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /carrier_types/1
+  # DELETE /carrier_types/1.json
+  def destroy
+    @carrier_type.destroy
+
+    respond_to do |format|
+      format.html { redirect_to carrier_types_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def prepare_options
+    if defined?(EnjuCirculation)
+      @checkout_types = CheckoutType.select([:id, :display_name, :position])
+    end
   end
 end
--- app/models/carrier_type.rb
+++ app/models/carrier_type.rb
@@ -4,8 +4,10 @@ class CarrierType < ActiveRecord::Base
   default_scope :order => "carrier_types.position"
   has_many :manifestations
   if defined?(EnjuCirculation)
+    attr_accessible :carrier_type_has_checkout_types_attributes
     has_many :carrier_type_has_checkout_types, :dependent => :destroy
     has_many :checkout_types, :through => :carrier_type_has_checkout_types
+    accepts_nested_attributes_for :carrier_type_has_checkout_types, :allow_destroy => true, :reject_if => :all_blank
   end
 
   def mods_type
