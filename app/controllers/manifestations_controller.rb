a1cd141c5747975df8f9cdfccb8fac83ec59069d
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-05-15 15:34:18 +0900

    duplicated

--- app/controllers/manifestations_controller.rb
+++ app/controllers/manifestations_controller.rb
@@ -670,7 +670,6 @@ class ManifestationsController < ApplicationController
     @languages = Language.select([:id, :display_name, :position])
     @frequencies = Frequency.select([:id, :display_name, :position])
     @identifier_types = IdentifierType.select([:id, :display_name, :position])
-    @content_types = ContentType.select([:id, :display_name, :position])
     @nii_types = NiiType.select([:id, :display_name, :position]) if defined?(EnjuNii)
     if defined?(EnjuSubject)
       @subject_types = SubjectType.select([:id, :display_name, :position])
