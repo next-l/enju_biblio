37a9035d196429bf6f8b8927d0b150d012ae9fdd
Kosuke Tanabe <kosuke@e23.jp>
2013-11-10 14:14:21 +0900

    remove associated identifiers when destroying a manifestation nabeta/enju_leaf#81

--- app/models/manifestation.rb
+++ app/models/manifestation.rb
@@ -47,7 +47,7 @@ class Manifestation < ActiveRecord::Base
   belongs_to :frequency
   belongs_to :required_role, :class_name => 'Role', :foreign_key => 'required_role_id', :validate => true
   has_one :resource_import_result
-  has_many :identifiers
+  has_many :identifiers, :dependent => :destroy
   belongs_to :nii_type if defined?(EnjuNii)
   accepts_nested_attributes_for :creators, :allow_destroy => true, :reject_if => :all_blank
   accepts_nested_attributes_for :contributors, :allow_destroy => true, :reject_if => :all_blank
