5ad90ea4bb6186d20cf6de16ec9364ecbc03be2e
Kosuke Tanabe <kosuke@e23.jp>
2014-02-21 11:40:58 +0900

    'creates' table was not included

--- app/models/manifestation.rb
+++ app/models/manifestation.rb
@@ -346,7 +346,7 @@ class Manifestation < ActiveRecord::Base
     return nil if self.cached_numdocs < 5
     manifestation = nil
     # TODO: ヒット件数が0件のキーワードがあるときに指摘する
-    response = Manifestation.search(:include => [:creators, :contributors, :publishers, :items]) do
+    response = Manifestation.search(:include => [:creates, :realizes, :produces, :items]) do
       fulltext keyword if keyword
       order_by(:random)
       paginate :page => 1, :per_page => 1
