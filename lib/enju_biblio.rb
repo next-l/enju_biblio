8c4b4eb6db3cfdeda2bf48a64d7e4c8181e6f60e
Kosuke Tanabe <kosuke@e23.jp>
2014-03-10 11:04:34 +0900

    fixed authorization

--- lib/enju_biblio.rb
+++ lib/enju_biblio.rb
@@ -20,22 +20,22 @@ module EnjuBiblio
 
     def get_work
       @work = Manifestation.find(params[:work_id]) if params[:work_id]
-      authorize @work if @work
+      authorize @work, :show? if @work
     end
 
     def get_expression
       @expression = Manifestation.find(params[:expression_id]) if params[:expression_id]
-      authorize @expression if @expression
+      authorize @expression, :show? if @expression
     end
 
     def get_manifestation
       @manifestation = Manifestation.find(params[:manifestation_id]) if params[:manifestation_id]
-      authorize @manifestation if @manifestation
+      authorize @manifestation, :show? if @manifestation
     end
 
     def get_item
       @item = Item.find(params[:item_id]) if params[:item_id]
-      authorize @item if @item
+      authorize @item, :show? if @item
     end
 
     def get_carrier_type
@@ -44,15 +44,17 @@ module EnjuBiblio
 
     def get_agent
       @agent = Agent.find(params[:agent_id]) if params[:agent_id]
-      authorize @agent if @agent
+      authorize @agent, :show? if @agent
     end
 
     def get_series_statement
       @series_statement = SeriesStatement.find(params[:series_statement_id]) if params[:series_statement_id]
+      authorize @series_statement, :show? if @series_statement
     end
 
     def get_basket
       @basket = Basket.find(params[:basket_id]) if params[:basket_id]
+      authorize @basket, :show? if @basket
     end
   end
 end
