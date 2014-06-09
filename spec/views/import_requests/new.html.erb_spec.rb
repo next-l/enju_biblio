39fd954014fd512b95b45ca0bfdafb2a81cf36a4
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-09 13:51:06 +0900

    added spec files

---
+++ spec/views/import_requests/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "import_requests/new" do
+  before(:each) do
+    assign(:import_request, stub_model(ImportRequest,
+      :name => "MyString",
+      :display_name => "MyText",
+      :note => "MyText",
+      :position => 1
+    ).as_new_record)
+  end
+
+  it "renders new import_request form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => import_requests_path, :method => "post" do
+      assert_select "input#import_request_isbn", :name => "import_request[isbn]"
+    end
+  end
+end
