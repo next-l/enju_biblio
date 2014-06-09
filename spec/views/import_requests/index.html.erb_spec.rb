9ad357f036c8e1ccee75bd6a08566ba7e782261b
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-09 14:17:04 +0900

    fixed spec file

--- spec/views/import_requests/index.html.erb_spec.rb
+++ spec/views/import_requests/index.html.erb_spec.rb
@@ -28,6 +28,6 @@ describe "import_requests/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td:nth-child(2)", /admin\n      1111111111\n/
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td:nth-child(3)", :text => "処理待ち".to_s
+    assert_select "tr>td:nth-child(3)", :text => localized_state('pending')
   end
 end
