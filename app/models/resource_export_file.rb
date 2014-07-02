b8c52b1b988ac888023b982215c71d819b6f33fa
Kosuke Tanabe <nabeta@fastmail.fm>
2014-07-02 18:04:35 +0900

    added statesman methods

--- app/models/resource_export_file.rb
+++ app/models/resource_export_file.rb
@@ -1,9 +1,17 @@
 class ResourceExportFile < ActiveRecord::Base
+  include Statesman::Adapters::ActiveRecordModel
   belongs_to :user
   has_attached_file :resource_export
   validates_attachment_content_type :resource_export, :content_type => /\Atext\/plain\Z/
   validates :user, presence: true
   attr_accessor :mode
+
+  def state_machine
+    ResourceImportFileStateMachine.new(self, transition_class: ResourceImportFileTransition)
+  end
+
+  delegate :can_transition_to?, :transition_to!, :transition_to, :current_state,
+    to: :state_machine
 
   def export
     sleep 10
@@ -20,6 +28,11 @@ class ResourceExportFile < ActiveRecord::Base
         body: I18n.t('export.export_completed')
       )
     end
+  end
+
+  private
+  def self.transition_class
+    ResourceExportFileTransition
   end
 end
 
