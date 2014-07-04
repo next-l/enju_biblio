ca259232f4b37aa26a93c302c4d175639ba819e9
Kosuke Tanabe <nabeta@fastmail.fm>
2014-07-04 09:39:33 +0900

    fixed spec file

--- spec/views/resource_export_files/show.html.erb_spec.rb
+++ spec/views/resource_export_files/show.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "resource_export_files/show" do
   before(:each) do
     @resource_export_file = assign(:resource_export_file, stub_model(ResourceExportFile))
+    view.stub(:current_user).and_return(User.find(1))
   end
 
   it "renders attributes in <p>" do
