0c4f83d28d46c51e9b922bf02d30dbcf22d8a0d5
Kosuke Tanabe <nabeta@fastmail.fm>
2014-07-25 13:29:19 +0900

    fixed factory file

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -8,6 +8,7 @@ FactoryGirl.define do
     f.user_group {UserGroup.first}
     f.required_role {Role.find_by_name('User')}
     f.sequence(:user_number){|n| "user_number_#{n}"}
+    f.library {Library.find(2)}
   end
 
   factory :librarian, :class => User do |f|
@@ -19,6 +20,7 @@ FactoryGirl.define do
     f.user_group {UserGroup.first}
     f.required_role {Role.find_by_name('User')}
     f.sequence(:user_number){|n| "user_number_#{n}"}
+    f.library {Library.find(2)}
   end
 
   factory :user, :class => User do |f|
@@ -30,6 +32,7 @@ FactoryGirl.define do
     f.user_group {UserGroup.first}
     f.required_role {Role.find_by_name('User')}
     f.sequence(:user_number){|n| "user_number_#{n}"}
+    f.library {Library.find(2)}
   end
 
   factory :invalid_user, :class => User do |f|
