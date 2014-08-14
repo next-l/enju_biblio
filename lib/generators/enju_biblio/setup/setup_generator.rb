09c9768b6b36fa28559a0675bbbf30ed91d8d2b5
Kosuke Tanabe <nabeta@fastmail.fm>
2014-08-14 21:14:08 +0900

    use RDA terms for CarrierType next-l/enju_leaf#300

--- lib/generators/enju_biblio/setup/setup_generator.rb
+++ lib/generators/enju_biblio/setup/setup_generator.rb
@@ -1,8 +1,10 @@
 class EnjuBiblio::SetupGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+  argument :file, :type => :string, :default => "all"
 
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_biblio")
+    return if file == 'fixture'
     rake("enju_biblio_engine:install:migrations")
     append_to_file("config/schedule.rb", File.open(File.expand_path('../templates', __FILE__) + '/config/schedule.rb').read)
   end
