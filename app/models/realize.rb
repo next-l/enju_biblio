28717d14f465e4ff2bafff752254c0fe077931a7
Kosuke Tanabe <nabeta@fastmail.fm>
2014-08-16 17:22:29 +0900

    added touch option next-l/enju_leaf#320

--- app/models/realize.rb
+++ app/models/realize.rb
@@ -1,7 +1,7 @@
 class Realize < ActiveRecord::Base
   attr_accessible :agent_id, :expression_id, :realize_type_id, :position
   belongs_to :agent
-  belongs_to :expression, class_name: 'Manifestation', foreign_key: 'expression_id'
+  belongs_to :expression, class_name: 'Manifestation', foreign_key: 'expression_id', touch: true
   belongs_to :realize_type
 
   validates_associated :agent, :expression
