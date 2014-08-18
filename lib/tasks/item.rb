f03833e2d317266ce50d1afbab347f1e25a29681
Kosuke Tanabe <nabeta@fastmail.fm>
2014-08-18 11:51:05 +0900

    updated upgrade task

--- lib/tasks/item.rb
+++ lib/tasks/item.rb
@@ -1,7 +1,7 @@
 def update_item
   Exemplify.find_each do |exemplify|
     if exemplify.item
-      exemplify.item.update_column(:manifestation_id, exemplify.manifestation_id)
+      exemplify.item.update_column(:manifestation_id, exemplify.manifestation_id) unless exemplify.item.manifestation_id
     end
   end
 end
