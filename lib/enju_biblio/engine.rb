d5c7f255dd5e6c33671db33e77de76195658b45b
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-21 04:01:53 +0900

    require coveralls

--- lib/enju_biblio/engine.rb
+++ lib/enju_biblio/engine.rb
@@ -15,7 +15,6 @@ begin
   require 'charlock_holmes/string'
 rescue LoadError
 end
-require "protected_attributes" if Rails::VERSION::MAJOR == 4
 
 module EnjuBiblio
   class Engine < ::Rails::Engine
