67c931e367a13a612d8cb19928639444b7ef3d64
Kosuke Tanabe <nabeta@fastmail.fm>
2015-01-14 00:44:31 +0900

    deliver -> deliver_later

--- app/controllers/manifestations_controller.rb
+++ app/controllers/manifestations_controller.rb
@@ -356,7 +356,7 @@ class ManifestationsController < ApplicationController
     case params[:mode]
     when 'send_email'
       if user_signed_in?
-        Notifier.manifestation_info(current_user.id, @manifestation.id).deliver
+        Notifier.manifestation_info(current_user.id, @manifestation.id).deliver_later
         flash[:notice] = t('page.sent_email')
         redirect_to @manifestation
         return
