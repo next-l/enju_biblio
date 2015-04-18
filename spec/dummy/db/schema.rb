b8324caade642730528e1168964171a9264115d0
Kosuke Tanabe <nabeta@fastmail.fm>
2015-04-19 01:52:36 +0900

    fixed dummy migration files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -474,14 +474,16 @@ ActiveRecord::Schema.define(version: 20150305105231) do
 
   create_table "event_export_files", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "event_export_file_name"
-    t.string   "event_export_content_type"
-    t.integer  "event_export_file_size"
-    t.datetime "event_export_updated_at"
     t.datetime "executed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "event_export_id"
+    t.string   "event_export_file_name"
+    t.integer  "event_export_size"
+    t.string   "event_export_content_type"
   end
+
+  add_index "event_export_files", ["event_export_id"], name: "index_event_export_files_on_event_export_id"
 
   create_table "event_import_file_transitions", force: :cascade do |t|
     t.string   "to_state"
@@ -1486,14 +1488,16 @@ ActiveRecord::Schema.define(version: 20150305105231) do
 
   create_table "user_export_files", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "user_export_file_name"
-    t.string   "user_export_content_type"
-    t.integer  "user_export_file_size"
-    t.datetime "user_export_updated_at"
     t.datetime "executed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_export_id"
+    t.string   "user_export_file_name"
+    t.integer  "user_export_size"
+    t.string   "user_export_content_type"
   end
+
+  add_index "user_export_files", ["user_export_id"], name: "index_user_export_files_on_user_export_id"
 
   create_table "user_group_has_checkout_types", force: :cascade do |t|
     t.integer  "user_group_id",                                   null: false
