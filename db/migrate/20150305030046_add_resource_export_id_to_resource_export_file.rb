e6866b4ff3674afb6f37bfb8b57c2ea9cd11e563
Kosuke Tanabe <nabeta@fastmail.fm>
2015-04-25 14:32:15 +0900

    fixed migration file

--- db/migrate/20150305030046_add_resource_export_id_to_resource_export_file.rb
+++ db/migrate/20150305030046_add_resource_export_id_to_resource_export_file.rb
@@ -2,7 +2,7 @@ class AddResourceExportIdToResourceExportFile < ActiveRecord::Migration
   def change
     add_column :resource_export_files, :resource_export_id, :string
     add_column :resource_export_files, :resource_export_size, :integer
-    rename_column :resource_export_files, :resource_export_file_name, :resource_export_filename
+    add_column :resource_export_files, :resource_export_filename
     add_index :resource_export_files, :resource_export_id
   end
 end
