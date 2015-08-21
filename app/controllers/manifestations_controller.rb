d345d20446699a016501fa794c4581438ac43ae8
Kosuke Tanabe <nabeta@fastmail.fm>
2015-08-21 13:09:43 +0900

    use repository_content field next-l/enju_leaf#704

--- app/controllers/manifestations_controller.rb
+++ app/controllers/manifestations_controller.rb
@@ -133,6 +133,7 @@ class ManifestationsController < ApplicationController
         fulltext query unless query.blank?
         order_by sort[:sort_by], sort[:order] unless oai_search
         order_by :updated_at, :desc if oai_search
+        with(:repository_content).equal_to true if oai_search
         if defined?(EnjuSubject)
           with(:subject_ids).equal_to subject.id if subject
         end
@@ -155,7 +156,6 @@ class ManifestationsController < ApplicationController
             end
           end
         end
-        order_by sort[:sort_by], sort[:order] unless oai_search
         facet :reservable if defined?(EnjuCirculation)
       end
       search = make_internal_query(search)
--- lib/enju_biblio/version.rb
+++ lib/enju_biblio/version.rb
@@ -1,3 +1,3 @@
 module EnjuBiblio
-  VERSION = "0.1.0.pre68"
+  VERSION = "0.1.0.pre69"
 end
