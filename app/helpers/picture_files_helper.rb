c4706db2fdc0edba7bd2280e1e6d121b61ba2c4f
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-12 13:30:21 +0900

    fix image width and height

--- app/helpers/picture_files_helper.rb
+++ app/helpers/picture_files_helper.rb
@@ -3,13 +3,13 @@ module PictureFilesHelper
     case options[:size]
     when :medium
       if picture_file.picture.width.to_i >= 600
-        return image_tag picture_file_path(picture_file, format: :download, size: :medium), alt: "*", width: picture_file.picture.width(:medium), height: picture_file.picture.height(:medium)
+        return image_tag picture_file_path(picture_file, format: :download, size: :medium), alt: "*", width: 600
       end
     when :thumb
       if picture_file.picture.width.to_i >= 100
-        return image_tag picture_file_path(picture_file, format: :download, size: :thumb), alt: "*", width: picture_file.picture.width(:thumb), height: picture_file.picture.height(:thumb)
+        return image_tag picture_file_path(picture_file, format: :download, size: :thumb), alt: "*", width: 100
       end
     end
-    image_tag picture_file_path(picture_file, format: :download, size: :original), alt: "*", width: picture_file.picture.width, height: picture_file.picture.height
+    image_tag picture_file_path(picture_file, format: :download, size: :original), alt: "*", width: picture_file.picture_width, height: picture_file.picture_height
   end
 end
