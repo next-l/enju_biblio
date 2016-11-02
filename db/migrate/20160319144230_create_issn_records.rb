155835894314d368f26a6f227ca74c4366858ba9
Kosuke Tanabe <nabeta@fastmail.fm>
2016-11-03 04:53:33 +0900

    add "null: false" on issn_records

--- db/migrate/20160319144230_create_issn_records.rb
+++ db/migrate/20160319144230_create_issn_records.rb
@@ -1,7 +1,7 @@
 class CreateIssnRecords < ActiveRecord::Migration
   def change
     create_table :issn_records do |t|
-      t.string :body, index: true
+      t.string :body, index: true, null: false
       t.string :issn_type
       t.string :source
       t.integer :manifestation_id, index: true
