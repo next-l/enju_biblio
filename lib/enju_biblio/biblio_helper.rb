7336cb5133fd03f0cf512bc3c0a2bc6ef6f279e0
Kosuke Tanabe <nabeta@fastmail.fm>
2017-01-28 14:52:58 +0900

    display uploaded carrier_type icon next-l/enju_leaf#1243

--- lib/enju_biblio/biblio_helper.rb
+++ lib/enju_biblio/biblio_helper.rb
@@ -11,7 +11,7 @@ module EnjuBiblio
       when 'online_resource'
         image_tag('icons/monitor.png', size: '16x16', class: 'enju_icon', alt: carrier_type.display_name.localize)
       else
-        image_tag('icons/help.png', size: '16x16', class: 'enju_icon', alt: t('page.unknown'))
+        image_tag(carrier_type_path(carrier_type, format: :download), size: '16x16', class: 'enju_icon', alt: carrier_type.display_name.localize)
       end
     rescue NoMethodError
       image_tag('icons/help.png', size: '16x16', class: 'enju_icon', alt: t('page.unknown'))
