165a85664c716ca04dec0e7e464b7621b60ee955
Kosuke Tanabe <nabeta@fastmail.fm>
2017-10-09 02:19:50 +0900

    add enju_biblio:update generator

---
+++ lib/generators/enju_biblio/update/update_generator.rb
@@ -0,0 +1,11 @@
+class EnjuBiblio::UpdateGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  desc "Create files for updating Next-L Enju"
+
+  def copy_migration_files
+    generate('statesman:add_constraints_to_most_recent', 'AgentImportFile', 'AgentImportFileTransition')
+    generate('statesman:add_constraints_to_most_recent', 'AgentExportFile', 'AgentExportFileTransition')
+    generate('statesman:add_constraints_to_most_recent', 'ResourceImportFile', 'ResourceImportFileTransition')
+    generate('statesman:add_constraints_to_most_recent', 'ResourceExportFile', 'ResourceExportFileTransition')
+  end
+end
