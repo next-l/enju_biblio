fca6a9526ebd2bc147d620a03ebd5d0099238e8c
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-03 01:53:52 +0900

    add validation

--- app/models/issn_record_and_manifestation.rb
+++ app/models/issn_record_and_manifestation.rb
@@ -2,6 +2,7 @@ class IssnRecordAndManifestation < ApplicationRecord
   belongs_to :issn_record
   belongs_to :manifestation
   acts_as_list
+  validates :issn_record_id, uniqueness: {scope: :manifestation_id}
 end
 
 # == Schema Information
