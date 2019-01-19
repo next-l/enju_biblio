52f781eba38c9324fe8b1ab9ad69125dc3f27f07
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-20 01:39:05 +0900

    remove Statesman::Adapters::ActiveRecordTransition from ImportRequest

--- app/models/import_request_transition.rb
+++ app/models/import_request_transition.rb
@@ -1,5 +1,4 @@
 class ImportRequestTransition < ActiveRecord::Base
-  include Statesman::Adapters::ActiveRecordTransition
 
   
   belongs_to :import_request, inverse_of: :import_request_transitions
