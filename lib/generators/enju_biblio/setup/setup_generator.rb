202bb9c7816b9d28754473147141b95623acc9e5
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-21 00:52:25 +0900

    fix generator

--- lib/generators/enju_biblio/setup/setup_generator.rb
+++ lib/generators/enju_biblio/setup/setup_generator.rb
@@ -6,5 +6,8 @@ class EnjuBiblio::SetupGenerator < Rails::Generators::Base
     directory("db/fixtures", "db/fixtures/enju_biblio")
     return if file == 'fixture'
     append_to_file("config/schedule.rb", File.open(File.expand_path('../templates', __FILE__) + '/config/schedule.rb').read)
+    append_to_file "app/models/user.rb", <<EOS
+User.include(EnjuBiblio::EnjuUser)
+EOS
   end
 end
