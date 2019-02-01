f83c7ebb25621124421c5ea18b32742e940bf53c
Kosuke Tanabe <nabeta@fastmail.fm>
2019-02-02 03:33:04 +0900

    update spec files

--- spec/views/agent_types/new.html.erb_spec.rb
+++ spec/views/agent_types/new.html.erb_spec.rb
@@ -16,7 +16,7 @@ describe "agent_types/new" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", action: agent_types_path, method: "post" do
       assert_select "input#agent_type_name", name: "agent_type[name]"
-      assert_select "textarea#agent_type_display_name", name: "agent_type[display_name]"
+      assert_select "input#agent_type_display_name_en", name: "agent_type[display_name_en]"
       assert_select "textarea#agent_type_note", name: "agent_type[note]"
     end
   end
