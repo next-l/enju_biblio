57b668e75360a1eee1b609e7a54424232d0e5498
Kosuke Tanabe <nabeta@fastmail.fm>
2019-02-24 00:26:55 +0900

    fix displaying image files

--- app/helpers/picture_files_helper.rb
+++ app/helpers/picture_files_helper.rb
@@ -4,13 +4,9 @@ module PictureFilesHelper
 
     case options[:size]
     when :medium
-      if picture_file.picture_width.to_i >= 600
-        return image_tag picture_file.picture.variant(resize: '600x'), alt: "*", width: 600
-      end
+      return image_tag picture_file.picture.variant(resize: '600x'), alt: "*", width: picture_file.picture.metadata['width']
     when :thumb
-      if picture_file.picture_width.to_i >= 100
-        return image_tag picture_file.picture.variant(resize: '100x'), alt: "*", width: 100
-      end
+      return image_tag picture_file.picture.variant(resize: '100x'), alt: "*", width: picture_file.picture.metadata['width']
     end
     image_tag picture_file.picture, alt: "*", width: picture_file.picture.metadata['width'], height: picture_file.picture.metadata['height']
   end
--- app/models/doi_record.rb
+++ app/models/doi_record.rb
@@ -1,6 +1,24 @@
 class DoiRecord < ApplicationRecord
   belongs_to :manifestation
-  validates :body, presence: true
+  validates :body, presence: true, uniqueness: true, format: {with: /\A[0-9]{2}\.[0-9]{2,}\/.+\Z/}
+  before_save :normalize
+  before_save :set_display_body
+
+  strip_attributes
+
+  def normalize
+    url = URI.parse(body)
+    if url.host =~ /doi\.org\Z/
+      self.body = url.path.gsub(/\A\//, '').downcase
+    else
+      self.body = body.downcase
+    end
+  rescue URI::InvalidURIError
+  end
+
+  def set_display_body
+    self.display_body = body unless display_body
+  end
 end
 
 # == Schema Information
