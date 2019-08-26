2ea85514fb1a463329be6f48b5f2637413412427
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-27 02:05:47 +0900

    add webpacker files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -811,14 +811,6 @@ ActiveRecord::Schema.define(version: 2019_08_23_161828) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture_file_name"
-    t.string "picture_content_type"
-    t.integer "picture_file_size"
-    t.datetime "picture_updated_at"
-    t.text "picture_meta"
-    t.string "picture_fingerprint"
-    t.integer "picture_width"
-    t.integer "picture_height"
     t.index ["picture_attachable_id", "picture_attachable_type"], name: "index_picture_files_on_picture_attachable_id_and_type"
   end
 
