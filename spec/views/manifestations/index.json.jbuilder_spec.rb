d77d7ccaf1d208372daf2210e90418c7fcfc32bc
Kosuke Tanabe <nabeta@fastmail.fm>
2019-11-30 20:23:18 +0900

    add a spec file for json template

---
+++ spec/views/manifestations/index.json.jbuilder_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper.rb"
+
+describe "manifestations/index.json.builder" do
+  before(:each) do
+    manifestation = FactoryBot.create(:manifestation)
+    @manifestations = assign(:manifestations, [manifestation] )
+    @library_group = LibraryGroup.first
+    view.stub(:filtered_params).and_return(ActionController::Parameters.new(per_page: 50).permit([:per_page]))
+  end
+
+  it "should export JSON format" do
+    params[:format] = "rdf"
+    render
+    JSON.parse(rendered)['total_count'].to eq Manifestation.count
+  end
+end
+
