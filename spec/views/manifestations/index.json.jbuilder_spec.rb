4c6f354244749d7ec6738168042b986d7df83808
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-07 17:34:37 +0900

    fix spec file

--- spec/views/manifestations/index.json.jbuilder_spec.rb
+++ spec/views/manifestations/index.json.jbuilder_spec.rb
@@ -1,17 +1,18 @@
 require "rails_helper.rb"
 
-describe "manifestations/index.json.jbuilder" do
+describe "manifestations/index.json.jbuilder", solr: true do
   before(:each) do
     manifestation = FactoryBot.create(:manifestation)
     @manifestations = assign(:manifestations, [manifestation] )
     @library_group = LibraryGroup.first
+    @count = {query_result: Manifestation.search.total}
     view.stub(:filtered_params).and_return(ActionController::Parameters.new(per_page: 50).permit([:per_page]))
   end
 
   it "should export JSON format" do
-    params[:format] = "rdf"
+    params[:format] = "json"
     render
-    JSON.parse(rendered)['total_count'].to eq Manifestation.count
+    expect(JSON.parse(rendered)['total_count']).to eq Manifestation.search.total
   end
 end
 
