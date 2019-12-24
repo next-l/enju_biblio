87c9aa12b6d3feacab626328b8e70974540bbf6e
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-24 19:52:58 +0900

    allow transition from pending to failed

--- app/models/resource_export_file_state_machine.rb
+++ app/models/resource_export_file_state_machine.rb
@@ -6,7 +6,7 @@ class ResourceExportFileStateMachine
   state :completed
   state :failed
 
-  transition from: :pending, to: :started
+  transition from: :pending, to: [:started, :failed]
   transition from: :started, to: [:completed, :failed]
 
   after_transition(from: :pending, to: :started) do |resource_export_file|
--- app/views/manifestations/show.txt.ruby
+++ app/views/manifestations/show.txt.ruby
@@ -1,5 +1,5 @@
 Manifestation.csv_header(
   role: current_user_role_name
-).keys.to_csv(col_sep: "\t") + @manifestation.to_hash(
+).to_csv(col_sep: "\t") + @manifestation.to_hash(
   role: current_user_role_name
 ).values.to_csv(col_sep: "\t")
