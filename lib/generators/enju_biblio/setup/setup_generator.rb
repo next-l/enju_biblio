64550815d2c9e64abe0b434ecaeefe1d8a532908
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-30 20:35:47 +0900

    update generator

--- lib/generators/enju_biblio/setup/setup_generator.rb
+++ lib/generators/enju_biblio/setup/setup_generator.rb
@@ -5,6 +5,8 @@ class EnjuBiblio::SetupGenerator < Rails::Generators::Base
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_biblio")
     return if file == 'fixture'
+    inject_into_class 'app/controllers/application_controller.rb', ApplicationController,
+      "  include EnjuBiblio::Controller\n"
     append_to_file("config/schedule.rb", File.open(File.expand_path('../templates', __FILE__) + '/config/schedule.rb').read)
   end
 end
