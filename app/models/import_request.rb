cc593a508f449ceaa252e5142c4df250e4411b3f
Kosuke Tanabe <nabeta@fastmail.fm>
2020-01-04 21:09:48 +0900

    remove old plugin definitions

--- app/models/import_request.rb
+++ app/models/import_request.rb
@@ -3,12 +3,10 @@ class ImportRequest < ApplicationRecord
   default_scope { order('import_requests.id DESC') }
   belongs_to :manifestation, optional: true
   belongs_to :user
-  validates_presence_of :isbn
+  validates :isbn, presence: true
   validate :check_isbn
   #validate :check_imported, on: :create
   #validates_uniqueness_of :isbn, if: Proc.new{|request| ImportRequest.where("created_at > ?", 1.day.ago).collect(&:isbn).include?(request.isbn)}
-  enju_ndl_ndl_search if defined?(EnjuNdl)
-  enju_nii_cinii_books if defined?(EnjuNii)
 
   has_many :import_request_transitions, autosave: false
 
