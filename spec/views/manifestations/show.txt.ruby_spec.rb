66f2dbec98f84733985130c73507e49cf9a4a431
Kosuke Tanabe <nabeta@fastmail.fm>
2020-03-22 17:27:54 +0900

    update spec file

--- spec/views/manifestations/show.txt.ruby_spec.rb
+++ spec/views/manifestations/show.txt.ruby_spec.rb
@@ -11,6 +11,6 @@ describe "manifestations/show.txt.ruby" do
     allow(view).to receive(:policy).and_return double(create?: true)
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
+    rendered.should match(/text/)
   end
 end
