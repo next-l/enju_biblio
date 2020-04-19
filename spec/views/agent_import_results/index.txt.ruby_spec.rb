6e61a910734fe12440daea8bb97c903c3145e2ea
Kosuke Tanabe <nabeta@fastmail.fm>
2020-04-19 17:08:13 +0900

    add spec files

---
+++ spec/views/agent_import_results/index.txt.ruby_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "agent_import_results/index.txt.ruby" do
+  fixtures :all
+
+  before(:each) do
+    file = AgentImportFile.create(
+      agent_import: File.new("#{Rails.root}/../../examples/agent_import_file_sample1.tsv"),
+      user: users(:admin)
+    )
+    file.import_start
+    assign(:agent_import_file_id, file.id)
+    assign(:agent_import_results, AgentImportFile.find(file.id).agent_import_results)
+  end
+
+  it "renders a list of agent_import_results" do
+    render
+    expect(CSV.parse(rendered, headers: true, col_sep: "\t").first['full_name']).to eq 'フルネーム'
+  end
+end
