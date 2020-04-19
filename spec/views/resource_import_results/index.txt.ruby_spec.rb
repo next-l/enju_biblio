18ef72e1fcc320c6f61e6377f97481ff08a9c449
Kosuke Tanabe <nabeta@fastmail.fm>
2020-04-19 14:50:57 +0900

    fix header in resource_export_file TSV next-l/enju_leaf#1080

---
+++ spec/views/resource_import_results/index.txt.ruby_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "resource_import_results/index.txt.ruby" do
+  fixtures :all
+
+  before(:each) do
+    file = ResourceImportFile.create(
+      resource_import: File.new("#{Rails.root}/../../examples/resource_import_file_sample1.tsv"),
+      default_shelf_id: 3,
+      user: users(:admin)
+    )
+    file.import_start
+    assign(:resource_import_file_id, file.id)
+    assign(:resource_import_results, ResourceImportFile.find(file.id).resource_import_results)
+  end
+
+  it "renders a list of resource_import_results" do
+    render
+    puts rendered
+    expect(CSV.parse(rendered, headers: true, col_sep: "\t").first['original_title']).to eq 'タイトル'
+    expect(CSV.parse(rendered, headers: true, col_sep: "\t")[10]['original_title']).to eq 'test8'
+  end
+end
