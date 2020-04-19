19270d0f1a0586ffa70f02f43d4fbf87b6097588
Kosuke Tanabe <nabeta@fastmail.fm>
2020-04-19 19:01:06 +0900

    update spec files

--- spec/views/agent_import_results/show.html.erb_spec.rb
+++ spec/views/agent_import_results/show.html.erb_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-describe "resource_import_results/show" do
+describe "agent_import_results/show" do
   fixtures :all
 
   before(:each) do
-    @resource_import_result = resource_import_results(:one)
+    @agent_import_result = agent_import_results(:one)
     admin = User.find_by(username: 'enjuadmin')
     view.stub(:current_user).and_return(admin)
   end
 
-  it "renders a resource_import_result" do
+  it "renders a agent_import_result" do
     render
     expect(rendered).to match /MyString/
   end
 
-  it "renders even if a resource_import_file is deleted" do
-    #@resource_import_result.resource_import_file = nil
-    @resource_import_result.resource_import_file.delete
-    @resource_import_result.resource_import_file = nil
+  it "renders even if a agent_import_file is deleted" do
+    #@agent_import_result.agent_import_file = nil
+    @agent_import_result.agent_import_file.delete
+    @agent_import_result.agent_import_file = nil
     render
-    expect(rendered).to match /original_title/
+    expect(rendered).to match /MyString/
   end
 end
