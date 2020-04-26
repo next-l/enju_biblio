5d8d7f5736ea0c7a615fd84f0e7365936523db3e
Kosuke Tanabe <nabeta@fastmail.fm>
2020-04-26 18:46:59 +0900

    update spec files

--- spec/views/manifestation_custom_properties/index.html.erb_spec.rb
+++ spec/views/manifestation_custom_properties/index.html.erb_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "manifestation_custom_properties/index", type: :view do
 
   it "renders a list of manifestation_custom_properties" do
     render
-    assert_select "tr>td", text: "name1".to_s, count: 2
+    assert_select "tr>td", text: "name1".to_s, count: 1
     assert_select "tr>td", text: "MyText".to_s, count: 2
   end
 end
