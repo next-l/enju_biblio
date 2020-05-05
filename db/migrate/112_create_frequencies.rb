ea98071c9a1b7cd5874feb3c55cbd83983218187
Kosuke Tanabe <nabeta@fastmail.fm>
2020-05-05 22:25:27 +0900

    remove display_name from migration files

--- db/migrate/112_create_frequencies.rb
+++ db/migrate/112_create_frequencies.rb
@@ -2,7 +2,6 @@ class CreateFrequencies < ActiveRecord::Migration[5.2]
   def change
     create_table :frequencies, comment: '発行頻度' do |t|
       t.string :name, null: false
-      t.text :display_name
       t.text :note, comment: '備考'
       t.integer :position
 
