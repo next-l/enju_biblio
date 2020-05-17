fbd54689979a993214602c24347676fde4d5f438
Kosuke Tanabe <nabeta@fastmail.fm>
2020-05-18 03:37:38 +0900

    update spec files

--- app/models/item_custom_value.rb
+++ app/models/item_custom_value.rb
@@ -8,9 +8,9 @@ end
 #
 # Table name: item_custom_values
 #
-#  id                      :bigint           not null, primary key
-#  item_custom_property_id :bigint           not null
-#  item_id                 :bigint           not null
+#  id                      :integer          not null, primary key
+#  item_custom_property_id :integer          not null
+#  item_id                 :integer          not null
 #  value                   :text
 #  created_at              :datetime         not null
 #  updated_at              :datetime         not null
