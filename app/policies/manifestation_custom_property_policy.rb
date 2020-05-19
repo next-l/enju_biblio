db00069f473995cd6adb158afc945af8af88542a
Kosuke Tanabe <nabeta@fastmail.fm>
2020-05-19 23:23:29 +0900

    update policy files for custom properties

--- app/policies/manifestation_custom_property_policy.rb
+++ app/policies/manifestation_custom_property_policy.rb
@@ -8,14 +8,14 @@ class ManifestationCustomPropertyPolicy < ApplicationPolicy
   end
 
   def create?
-    true if user.try(:has_role?, 'Librarian')
+    true if user.try(:has_role?, 'Administrator')
   end
 
   def update?
-    true if user.try(:has_role?, 'Librarian')
+    true if user.try(:has_role?, 'Administrator')
   end
 
   def destroy?
-    true if user.try(:has_role?, 'Librarian')
+    true if user.try(:has_role?, 'Administrator')
   end
 end
