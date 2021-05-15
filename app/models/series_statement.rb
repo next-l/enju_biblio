af49cd033586fb85543947bf32c05c4dd4d03fd8
Kosuke Tanabe <nabeta@fastmail.fm>
2021-05-15 22:32:08 +0900

    add created_at and updated_at on SeriesStatement to Solr index

--- app/models/series_statement.rb
+++ app/models/series_statement.rb
@@ -18,6 +18,7 @@ class SeriesStatement < ApplicationRecord
     integer :manifestation_id
     integer :position
     integer :series_statement_merge_list_ids, multiple: true
+    time :created_at, :updated_at
   end
 
   attr_accessor :selected
